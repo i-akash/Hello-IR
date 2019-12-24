a352949dd15e37bbb99b24c82daac66bafb15ed1
i-akash <i.akash.se@gmail.com>
2019-12-24 16:14:05 +0600

    tf idf search

--- vector space model/vsm/VectorSpaceModelManager.py
+++ vector space model/vsm/VectorSpaceModelManager.py
@@ -22,7 +22,19 @@ class VectorSpaceModelManager(object):
     
     def update_tf_df(self):
         self.tf_idf.update_tf_idf_vector(self.vector_space_model)
-        print(self.vector_space_model.doc_list[0].get_tf_idf_vector())
+       
+    def isearch_tfidf(self,query):
+        query_vector=self.query_processor.process(query=query)
+        query_tf_vector=self.tf_idf.get_tf(p_vector=query_vector)
+    
+        doc_list=self.vector_space_model.get_doc()
+
+        for doc in doc_list:
+            doc_vector=doc.get_tf_idf_vector()
+
+            match_score=self.get_cosine_score(query_tf_vector,doc_vector)
+            print('Document: {0} has matching score: {1}'.format(doc.doc_name,match_score))
+
 
     def isearch(self,query):
         query_vector=self.query_processor.process(query=query)
@@ -33,21 +45,20 @@ class VectorSpaceModelManager(object):
             match_score=self.get_cosine_score(query_vector,doc_vector)
             print('Document: {0} has matching score: {1}'.format(doc.doc_name,match_score))
 
-
-        
-
+    
     def get_cosine_score(self,vector1,vector2):
         return np.dot(vector1,vector2)/np.sqrt(vector1.dot(vector1)*vector2.dot(vector2))
 
     
 
 vsmm=VectorSpaceModelManager()
-vsmm.add_doc(filename='../docs/doc3.txt')
-vsmm.add_doc(filename='../docs/doc2.txt')
 vsmm.add_doc(filename='../docs/doc1.txt')
+vsmm.add_doc(filename='../docs/doc2.txt')
+vsmm.add_doc(filename='../docs/doc3.txt')
 
 vsmm.update_tf_df()
 
-
-vsmm.isearch(query="hasina")
-
+print("-------------with tf idf--------------")
+vsmm.isearch_tfidf(query="Prime Minister Sheikh Hasina agreements two rivers men to be prepared")
+print("-------------without tf idf--------------")
+vsmm.isearch(query="Prime Minister Sheikh Hasina agreements two rivers men to be prepared")
