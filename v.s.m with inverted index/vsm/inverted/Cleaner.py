000b51688428c36c762b86abc3b7ec8093f45cdc
i-akash <i.akash.se@gmail.com>
2019-12-26 23:21:16 +0600

    inverted index at vsm

---
+++ v.s.m with inverted index/vsm/inverted/Cleaner.py
@@ -0,0 +1,24 @@
+from nltk.stem import PorterStemmer
+
+class Cleaner:
+    def __init__(self):
+        self.porterStemmer=PorterStemmer()
+
+    def __get_stemize(self,p_token=""):
+        l_token=self.porterStemmer.stem(p_token)
+        return l_token
+
+    def __is_stop_word(self,p_token=""):
+        return False
+
+
+    def get_clean(self,p_token_list=[]):
+        l_token_list=list()
+        for token in p_token_list:
+            if self.is_stop_word(p_token=token):
+                continue
+            token=self.get_stemize(p_token=token)
+            l_token_list.append(token)
+        
+        return l_token_list
+
--- v.s.m with inverted index/vsm/inverted/InvertedIndexModel.py
+++ v.s.m with inverted index/vsm/inverted/InvertedIndexModel.py
@@ -3,53 +3,58 @@
 class InvertedIndexModel(object):
     def __init__(self):
         self.inverted_index=dict()
+        self.total_doc_number=0
     
-    def add_term(self,term=""):
+    def __add_term(self,term=""):
         index_node=IndexNode(term=term)
-        posting_list=PostingList()
-        if index_node not in self.inverted_index:
-            self.inverted_index[index_node]=posting_list
+        self.inverted_index[term]=index_node
 
-
-    def add_posting(self,term="",doc_id="",tf=0,total_doc=0):
-        index_node=IndexNode(term=term)
-
-        if index_node in self.inverted_index:
-            posting_list=self.inverted_index[index_node]
-            posting_list.add_posting(doc_id=doc_id,tf=tf,total_doc=total_doc)
+    def add_posting(self,term="",doc_id="",tf_score=0):
+        if term not in self.inverted_index:
+            self.add_term(term=term)
+            
+        index_node=self.inverted_index[term]
+        self.total_doc_number=self.total_doc_number+1
+        index_node.add_posting(doc_id=doc_id,tf_socre=tf_score,total_doc_number=self.total_doc_number)
 
 
 class IndexNode(object):
     def __init__(self,term=""):
         self.term=term
+        self.idf_score=0
+        self.posting_list=PostingList()
+
+    def update_idf(self,total_doc_number=0,term_doc=0):
+        self.idf_score=log10((total_doc_number)/term_doc)
+
+    def add_posting(self,doc_id="",tf_socre=0,total_doc_number=0):
+        self.posting_list.add_posting(doc_id=doc_id,tf_socre=tf_socre)
+        self.update_idf(total_doc_number=total_doc_number,term_doc=self.posting_list.get_length())
+
+    # def __hash__(self):
+    #     return hash(self.term)
     
-    def __hash__(self):
-        return hash(self.term)
-    
-    def __eq__(self,other):
-        if isinstance(self,other.__class__):
-            return self.term==other.term
-        else :
-            return "NotImplemented"
+    # def __eq__(self,other):
+    #     if isinstance(self,other.__class__):
+    #         return self.term==other.term
+    #     else :
+    #         return "NotImplemented"
     
 
 class PostingList(object):
     def __init__(self):
         self.posting_list=list()
-        self.idf=0
-    
-    def add_posting(self,doc_id="",tf=0,total_doc=0):
-        posting_node=PostingNode(doc_id=doc_id,tf=tf)
+        
+    def add_posting(self,doc_id="",tf_socre=0):
+        posting_node=PostingNode(doc_id=doc_id,tf_socre=tf_socre)
         self.posting_list.append(posting_node)
-        self.update_idf(total_doc=total_doc,term_doc=len(self.posting_list))
-
-
-    def update_idf(self,total_doc=0,term_doc=0):
-        self.idf=log10((total_doc)/term_doc)
-
+        
+    def get_length(self):
+        return len(self.posting_list)
+    
 
 class PostingNode(object):
-    def __init__(self,doc_id="",tf=0):
+    def __init__(self,doc_id="",tf_socre=0):
         self.doc_id=doc_id
-        self.tf=tf
+        self.tf_socre=tf_socre
 
